5b861eddc3c8583c8ab3b968f808bac3bd08037d
Stefan Johnson <sj@scrimpycat.io>
2017-05-01 11:36:40 +1000

    Setup ecto repo

---
+++ apps/sherbet_service/config/test.exs
@@ -0,0 +1,13 @@
+use Mix.Config
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure database
+config :sherbet_service, Sherbet.Service.Repo,
+    adapter: Ecto.Adapters.Postgres,
+    username: "postgres",
+    password: "postgres",
+    database: "sherbet_service_test",
+    hostname: "localhost",
+    pool: Ecto.Adapters.SQL.Sandbox
---
+++ apps/sherbet_service/lib/sherbet.service/repo.ex
@@ -0,0 +1,3 @@
+defmodule Sherbet.Service.Repo do
+    use Ecto.Repo, otp_app: :sherbet_service
+end
