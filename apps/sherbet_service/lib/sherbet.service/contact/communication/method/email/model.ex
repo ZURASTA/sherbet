d07ac4e959c3d085a6c181561d913d61a643f1ce
Stefan Johnson <sj@scrimpycat.io>
2017-05-03 13:12:28 +1000

    Added email contact schema

---
+++ apps/sherbet_service/lib/sherbet.service/contact/communication/method/email/model.ex
@@ -0,0 +1,68 @@
+defmodule Sherbet.Service.Contact.Communication.Method.Email.Model do
+    use Ecto.Schema
+    import Ecto
+    import Ecto.Changeset
+    import Protecto
+    @moduledoc """
+      A model representing the different email contacts.
+
+      ##Fields
+
+      ###:id
+      Is the unique reference to the email contact entry. Is an `integer`.
+
+      ###:identity
+      Is the identity the email contact belongs to. Is an `UUID`.
+
+      ###:email
+      Is the email part of the contact. Is a `string`.
+
+      ###:verified
+      Indicates whether the email has been verified to belong to the given
+      identity. Is a `boolean`.
+    """
+
+    schema "emails" do
+        field :identity, Ecto.UUID
+        field :email, :string
+        field :verified, :boolean
+        timestamps()
+    end
+
+    @doc """
+      Builds a changeset for insertion based on the `struct` and `params`.
+
+      Enforces:
+      * `identity` field is required
+      * `email` field is required
+      * `email` field is a valid email
+      * `email` field is unique
+    """
+    def insert_changeset(struct, params \\ %{}) do
+        struct
+        |> cast(params, [:identity, :email, :verified])
+        |> validate_required([:identity, :email])
+        |> validate_email(:email)
+        |> unique_constraint(:email)
+    end
+
+    @doc """
+      Builds a changeset for update based on the `struct` and `params`.
+
+      Enforces:
+      * `identity` field is not empty
+      * `email` field is not empty
+      * `verified` field is not empty
+      * `email` field is a valid email
+      * `email` field is unique
+    """
+    def update_changeset(struct, params \\ %{}) do
+        struct
+        |> cast(params, [:identity, :email, :verified])
+        |> validate_emptiness(:identity)
+        |> validate_emptiness(:email)
+        |> validate_emptiness(:verified)
+        |> validate_email(:email)
+        |> unique_constraint(:email)
+    end
+end
