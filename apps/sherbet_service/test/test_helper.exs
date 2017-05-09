f95d1f996c168b73497e6a196d89a9ef23d82828
Stefan Johnson <sj@scrimpycat.io>
2017-05-09 16:48:48 +1000

    Start gobstopper service and manage all repos

--- apps/sherbet_service/test/test_helper.exs
+++ apps/sherbet_service/test/test_helper.exs
@@ -1,1 +1,7 @@
+Application.ensure_all_started(:gobstopper_service)
+Application.ensure_all_started(:ecto)
+
 ExUnit.start()
+
+Ecto.Adapters.SQL.Sandbox.mode(Sherbet.Service.Repo, :manual)
+Ecto.Adapters.SQL.Sandbox.mode(Gobstopper.Service.Repo, :manual)
