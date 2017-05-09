8a449312b9c16663098adae189f016aa31dee0af
Stefan Johnson <sj@scrimpycat.io>
2017-05-09 16:48:16 +1000

    Setup case template for tests

---
+++ apps/sherbet_service/test/support/case.ex
@@ -0,0 +1,28 @@
+defmodule Sherbet.Service.Case do
+    @moduledoc """
+      This module defines the test case to be used by all tests.
+    """
+
+    use ExUnit.CaseTemplate
+
+    using do
+        quote do
+            import Ecto
+            import Ecto.Changeset
+            import Ecto.Query
+            use Defecto, repo: Sherbet.Service.Repo
+        end
+    end
+
+    setup tags do
+        :ok = Ecto.Adapters.SQL.Sandbox.checkout(Sherbet.Service.Repo)
+        :ok = Ecto.Adapters.SQL.Sandbox.checkout(Gobstopper.Service.Repo)
+
+        unless tags[:async] do
+            Ecto.Adapters.SQL.Sandbox.mode(Sherbet.Service.Repo, { :shared, self() })
+            Ecto.Adapters.SQL.Sandbox.mode(Gobstopper.Service.Repo, { :shared, self() })
+        end
+
+        :ok
+    end
+end
