f40256f3d0e992d90671e40c33b472a6460f9873
Stefan Johnson <sj@scrimpycat.io>
2017-05-10 00:54:06 +1000

    Test email model

---
+++ apps/sherbet_service/test/sherbet.service/contact/communication/method/email/model_test.exs
@@ -0,0 +1,85 @@
+defmodule Sherbet.Service.Contact.Communication.Method.Email.ModelTest do
+    use Sherbet.Service.Case
+
+    alias Sherbet.Service.Contact.Communication.Method.Email
+
+    @valid_model %Email.Model{
+        identity: Ecto.UUID.generate(),
+        email: "foo@test",
+        verified: true
+    }
+
+    test "empty" do
+        refute_change(%Email.Model{}, %{}, :insert_changeset)
+    end
+
+    test "only identity" do
+        refute_change(%Email.Model{}, %{ identity: @valid_model.identity }, :insert_changeset)
+
+        assert_change(@valid_model, %{ identity: Ecto.UUID.generate() }, :update_changeset)
+    end
+
+    test "only email" do
+        refute_change(%Email.Model{}, %{ email: @valid_model.email }, :insert_changeset)
+
+        assert_change(@valid_model, %{ email: "foo@bar" }, :update_changeset)
+    end
+
+    test "only verified" do
+        refute_change(%Email.Model{}, %{ verified: @valid_model.verified }, :insert_changeset)
+
+        assert_change(@valid_model, %{ verified: false }, :update_changeset)
+    end
+
+    test "without identity" do
+        refute_change(@valid_model, %{ identity: nil }, :insert_changeset)
+    end
+
+    test "without email" do
+        refute_change(@valid_model, %{ email: nil }, :insert_changeset)
+    end
+
+    test "without verified" do
+        assert_change(@valid_model, %{ verified: nil }, :insert_changeset)
+    end
+
+    test "valid model" do
+        assert_change(@valid_model, %{}, :insert_changeset)
+
+        assert_change(@valid_model, %{}, :update_changeset)
+    end
+
+    test "email formatting" do
+        refute_change(@valid_model, %{ email: "test" }, :insert_changeset)
+        |> assert_error_value(:email, { "should contain a local part and domain separated by '@'", [validation: :email] })
+
+        refute_change(@valid_model, %{ email: "@" }, :insert_changeset)
+        |> assert_error_value(:email, { "should contain a local part and domain separated by '@'", [validation: :email] })
+
+        refute_change(@valid_model, %{ email: "test@" }, :insert_changeset)
+        |> assert_error_value(:email, { "should contain a local part and domain separated by '@'", [validation: :email] })
+
+        refute_change(@valid_model, %{ email: "test" }, :update_changeset)
+        |> assert_error_value(:email, { "should contain a local part and domain separated by '@'", [validation: :email] })
+
+        refute_change(@valid_model, %{ email: "@" }, :update_changeset)
+        |> assert_error_value(:email, { "should contain a local part and domain separated by '@'", [validation: :email] })
+
+        refute_change(@valid_model, %{ email: "test@" }, :update_changeset)
+        |> assert_error_value(:email, { "should contain a local part and domain separated by '@'", [validation: :email] })
+    end
+
+    test "uniqueness" do
+        email = Sherbet.Service.Repo.insert!(@valid_model)
+
+        assert_change(@valid_model, %{ identity: Ecto.UUID.generate(), email: @valid_model.email }, :insert_changeset)
+        |> assert_insert(:error)
+        |> assert_error_value(:email, { "has already been taken", [] })
+
+        assert_change(@valid_model, %{ identity: Ecto.UUID.generate(), email: "foo@bar" }, :insert_changeset)
+        |> assert_insert(:ok)
+
+        assert_change(@valid_model, %{ identity: @valid_model.identity, email: "foo@foo" }, :insert_changeset)
+        |> assert_insert(:ok)
+    end
+end
