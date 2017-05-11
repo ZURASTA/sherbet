65a6c6cac4a9723ea11e274d2a380f907f0fb374
Stefan Johnson <sj@scrimpycat.io>
2017-05-12 02:56:14 +1000

    Handle primary_contact and make_primary requests

--- apps/sherbet_service/lib/sherbet.service/contact.ex
+++ apps/sherbet_service/lib/sherbet.service/contact.ex
@@ -9,10 +9,12 @@ defmodule Sherbet.Service.Contact do
 
     def handle_call({ :add, { type, contact }, identity }, _from, state), do: { :reply, Communication.add(type, contact, identity), state }
     def handle_call({ :remove, { type, contact }, identity }, _from, state), do: { :reply, Communication.remove(type, contact, identity), state }
+    def handle_call({ :make_primary, { type, contact }, identity }, _from, state), do: { :reply, Communication.make_primary(type, contact, identity), state }
     def handle_call({ :request_removal, { type, contact } }, _from, state), do: { :reply, Communication.request_removal(type, contact), state }
     def handle_call({ :finalise_removal, { type, contact, key } }, _from, state), do: { :reply, Communication.finalise_removal(type, contact, key), state }
     def handle_call({ :verified?, { type, contact }, identity }, _from, state), do: { :reply, Communication.verified?(type, contact, identity), state }
     def handle_call({ :request_verification, { type, contact }, identity }, _from, state), do: { :reply, Communication.request_verification(type, contact, identity), state }
     def handle_call({ :finalise_verification, { type, contact, key }, identity }, _from, state), do: { :reply, Communication.finalise_verification(type, contact, key, identity), state }
     def handle_call({ :contacts, { type }, identity }, _from, state), do: { :reply, Communication.contacts(type, identity), state }
+    def handle_call({ :primary_contact, { type }, identity }, _from, state), do: { :reply, Communication.primary_contact(type, identity), state }
 end
