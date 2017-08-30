96944032458690dba398433309ead9932cc326b6
jiepu2 <pujiezhi@gmail.com>
2017-08-30 11:51:07 +1000

    Config changed back

--- apps/sherbet_service/config/config.exs
+++ apps/sherbet_service/config/config.exs
@@ -1,9 +1,7 @@
 use Mix.Config
 
 config :sherbet_service,
-    ecto_repos: [Sherbet.Service.Repo],
-    mobile_key_length: 8,
-    email_key_length: 32
+    ecto_repos: [Sherbet.Service.Repo]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
