b2023719453e8c882b818fff66502acd4407b85d
Stefan Johnson <sj@scrimpycat.io>
2018-04-14 20:21:17 +1000

    Add server match expressions for blueprint graph

---
+++ .servers.exs
@@ -0,0 +1,3 @@
+[
+    { { :call, _, { :call, _, { :remote, _, { :atom, _, Access }, { :atom, _, :get } }, [{ :var, _, _ }, { :atom, _, :server }, { :atom, _, _ }] }, [{ :atom, _, name }] }, name }
+]
